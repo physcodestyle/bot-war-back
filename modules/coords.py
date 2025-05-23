417037f032f2843e4c6931c0064c581c596ea5e5
Doka Dog <hi@doka.guide>
2025-05-23 17:29:34 +0300

    Add basic classes for navigation

---
+++ modules/coords.py
@@ -0,0 +1,8 @@
+class Coords:
+    def __init__(self, top: int, left: int):
+        self.top = top
+        self.left = left
+    
+
+    def get(self):
+        return (self.top, self.left)
---
+++ modules/map.py
@@ -0,0 +1,10 @@
+from modules.size import Size
+
+
+class Map:
+    def __init__(self, size: Size, objects: list[Size]):
+        self.size = size
+        self.objects = objects
+
+
+    
