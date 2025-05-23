9801fc789039fd1bffb62a09e9b95999ebdaab74
Doka Dog <hi@doka.guide>
2025-05-23 16:47:12 +0300

    Add basic http server

--- modules/api.py
+++ modules/api.py
@@ -1,8 +1,23 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
+from json import dumps
 
 
 class RestHandler(SimpleHTTPRequestHandler):
-    pass
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        response = {
+            'response': f'Server received on address "{self.path}" POST data: {post_data.decode("utf-8")}',
+        }
+        self.create_response(response)
+        
+        
+    def create_response(self, response):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-Length', str(len(dumps(response))))
+        self.end_headers()
+        self.wfile.write(str(response).encode('utf-8'))
 
 
 def run(settings):
